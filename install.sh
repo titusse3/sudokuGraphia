52745d0207db8548363b3fe2dcc772d0d954e659
titusse3 <titusse3.rateaux@gmail.com>
2024-01-08 13:04:40 +0100

    fix on installation and uninstall

---
+++ install.sh
@@ -0,0 +1,11 @@
+# Mise à jour de pip
+python -m pip install --upgrade pip
+
+# Installation de PyQt6
+pip install PyQt6
+
+# Installation de matplotlib
+pip install matplotlib
+
+# Installation de networkx
+pip install networkx
---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+# Désinstallation de PyQt6
+pip uninstall PyQt6 -y
+
+# Désinstallation de matplotlib
+pip uninstall matplotlib -y
+
+# Désinstallation de networkx
+pip uninstall networkx -y
